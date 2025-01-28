f71d82537484083d0a9a2257a7eb0eeea1f4afdd
pushkar-vyas <pushkarvyas204@gmail.com>
2025-01-28 11:55:14 +0530

    new commit

--- server/routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const {generateImage} = require('../controllers/imageController')
-const {userAuth} = require('../middlewares/auth')
-
-const imageRouter = express.Router()
-
-imageRouter.post('/generate-image',userAuth,generateImage)
-
-module.exports = imageRouter
